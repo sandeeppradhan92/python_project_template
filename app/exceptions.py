0bab7ce0be88a2b54748bb1556ca689bc9921851
Sandeep Kumar Pradhan <sandeeppradhan92@gmail.com>
2020-11-06 22:07:12 +0530

    Add files via upload

---
+++ app/exceptions.py
@@ -0,0 +1,2 @@
+class CommandExecuteException(Exception):
+    pass
---
+++ app/log.py
@@ -0,0 +1,44 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+
+from flask import has_request_context, request
+
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        if has_request_context():
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+        else:
+            record.url = None
+            record.remote_addr = None
+
+        return super().format(record)
+
+
+formatter = RequestFormatter(
+    '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+    '%(levelname)s in %(module)s: %(message)s'
+)
+
+
+def create_log_dir(log_dir):
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+
+def get_stream_handler():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
+    return stream_handler
+
+
+def get_file_handler(log_dir):
+    file_handler = TimedRotatingFileHandler(os.path.join(log_dir, 'app.log'),
+                                            when='midnight', backupCount=10)
+    file_handler.suffix = "%Y-%m-%d"
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+    return file_handler
